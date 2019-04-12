4126adb3a7a34d44ec98f0485eda391baf369955
Yifan-Tang <851963538@qq.com>
2019-04-12 13:24:53 -0700

    Initial Commit

---
+++ WordScapeSolver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScapeSolver
+//
+//  Created by 汤逸凡 on 2019/4/12.
+//  Copyright © 2019 汤逸凡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
